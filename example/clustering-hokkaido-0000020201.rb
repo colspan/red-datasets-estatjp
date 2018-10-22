15b640c989bb90fca6d77b269972b5e7f62af42a
Kunihiko MIYOSHI <miyoshik@gmail.com>
2018-10-22 20:35:38 +0900

    add examples

---
+++ example/clustering-hokkaido-0000020201.rb
@@ -0,0 +1,53 @@
+#!/usr/bin/env ruby -Ku
+
+# clustering communes in Hokkaido by statistics of population
+# e-Stat 0000020201 Ａ　人口・世帯
+
+require "bundler/setup"
+require "kmeans-clusterer"
+require "daru"
+
+require "estat-jp"
+require_relative "estat-config"
+
+estat = Datasets::Estat::EstatAPI.new(
+  "0000020201", # Ａ　人口・世帯
+  skip_parent_area: true,
+  skip_child_area: false,
+  skip_nil_column: true,
+  skip_nil_row: false,
+  cat: ["A1101"], # A1101_人口総数
+)
+
+# prepare for clustering
+indices = []
+rows = []
+map_id_name = {}
+estat.each do |record|
+  # 北海道に限定する
+  next unless record.id.to_s.start_with? "01"
+  indices << record.id
+  rows << record.values
+  map_id_name[record.id] = record.name
+end
+
+# create dataframe
+df = Daru::DataFrame.rows(rows, order: estat.schema, index: indices)
+
+# rate of change
+rate_of_change_df = df / df[df.vectors.to_a[0]]
+
+# do clustering
+k = 12 # Number of clusters to find
+kmeans = KMeansClusterer.run(
+  k,
+  rate_of_change_df.to_matrix.to_a,
+  labels: rate_of_change_df.index.to_a,
+  runs: 100,
+)
+puts ["id", "name", "cluster"].join(",")
+kmeans.clusters.each do |cluster|
+  cluster.points.each do |p|
+    puts [p.label, map_id_name[p.label], cluster.id].join(",")
+  end
+end
---
+++ example/clustering-hokkaido-0000020203.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby -Ku
+
+# clustering communes in Hokkaido by statistics of economy
+# e-Stat 0000020203 C 経済基盤
+
+require "bundler/setup"
+require "kmeans-clusterer"
+
+require "estat-jp"
+require_relative "estat-config"
+
+estat = Datasets::Estat::EstatAPI.new(
+  "0000020203", # Ｃ　経済基盤
+  skip_parent_area: false,
+  skip_child_area: true,
+  skip_nil_column: false,
+  skip_nil_row: true,
+  cat: ["C120110", "C120120"], # C120110_課税対象所得, C120120_納税義務者数（所得割）
+  time_range: -4..-1, # 2013〜2016年 (末尾から4〜1件目)
+)
+
+# prepare for clustering
+labels = []
+rows = []
+map_id_name = {}
+estat.each do |record|
+  # 北海道に限定する
+  next unless record.id.to_s.start_with? "01"
+  labels << record.id
+  rows << record.values
+  map_id_name[record.id] = record.name
+end
+
+puts estat.schema
+
+# do clustering
+k = 12 # Number of clusters to find
+kmeans = KMeansClusterer.run k, rows, labels: labels, runs: 100
+puts ["id", "name", "cluster"].join(",")
+kmeans.clusters.each do |cluster|
+  # puts "Cluster #{cluster.id}"
+  # puts "Center of Cluster: #{cluster.centroid}"
+  # puts "Cities in Cluster: " + cluster.points.map { |c| c.label }.join(",")
+  cluster.points.each do |p|
+    puts [p.label, map_id_name[p.label], cluster.id].join(",")
+  end
+end
