50f6be16741e2a781839ec4a3afe0f3b6f922db6
Kunihiko MIYOSHI <miyoshik@gmail.com>
2019-03-03 16:34:07 +0900

    format by rubocop

--- example/clustering-all.rb
+++ example/clustering-all.rb
@@ -2,42 +2,42 @@
 
 # clustering all communes by all available columns
 
-require "bundler/setup"
-require "kmeans-clusterer"
+require 'bundler/setup'
+require 'kmeans-clusterer'
 
-require "estat-jp"
-require_relative "estat-config"
+require 'estat-jp'
+require_relative 'estat-config'
 
 # 市区町村データ 基礎データ（廃置分合処理済）
 # https://www.e-stat.go.jp/stat-search/database?page=1&layout=datalist&toukei=00200502&tstat=000001111376&cycle=8&tclass1=000001111380&second=1&second2=1
 estat_datasets_essential = [
-  {data_id: "0000020201", name: "Ａ　人口・世帯"},
-  {data_id: "0000020202", name: "Ｂ　自然環境"},
-  {data_id: "0000020203", name: "Ｃ　経済基盤"},
-  {data_id: "0000020204", name: "Ｄ　行政基盤"},
-  {data_id: "0000020205", name: "Ｅ　教育"},
-  {data_id: "0000020206", name: "Ｆ　労働"},
-  {data_id: "0000020207", name: "Ｇ　文化・スポーツ"},
-  {data_id: "0000020208", name: "Ｈ　居住"},
-  {data_id: "0000020209", name: "Ｉ　健康・医療"},
-  {data_id: "0000020210", name: "Ｊ　福祉・社会保障"},
-  {data_id: "0000020211", name: "Ｋ　安全"},
+  { data_id: '0000020201', name: 'Ａ　人口・世帯' },
+  { data_id: '0000020202', name: 'Ｂ　自然環境' },
+  { data_id: '0000020203', name: 'Ｃ　経済基盤' },
+  { data_id: '0000020204', name: 'Ｄ　行政基盤' },
+  { data_id: '0000020205', name: 'Ｅ　教育' },
+  { data_id: '0000020206', name: 'Ｆ　労働' },
+  { data_id: '0000020207', name: 'Ｇ　文化・スポーツ' },
+  { data_id: '0000020208', name: 'Ｈ　居住' },
+  { data_id: '0000020209', name: 'Ｉ　健康・医療' },
+  { data_id: '0000020210', name: 'Ｊ　福祉・社会保障' },
+  { data_id: '0000020211', name: 'Ｋ　安全' }
 ]
 
 # 市区町村データ 社会生活統計指標（廃置分合処理済）
 # https://www.e-stat.go.jp/stat-search/database?page=1&layout=datalist&toukei=00200502&tstat=000001111376&cycle=8&tclass1=000001111381&second=1&second2=1
 estat_datasets_social_life = [
-  {data_id: "0000020301", name: "Ａ　人口・世帯"},
-  {data_id: "0000020302", name: "Ｂ　自然環境"},
-  {data_id: "0000020303", name: "Ｃ　経済基盤"},
-  {data_id: "0000020304", name: "Ｄ　行政基盤"},
-  {data_id: "0000020305", name: "Ｅ　教育"},
-  {data_id: "0000020306", name: "Ｆ　労働"},
-  {data_id: "0000020307", name: "Ｇ　文化・スポーツ"},
-  {data_id: "0000020308", name: "Ｈ　居住"},
-  {data_id: "0000020309", name: "Ｉ　健康・医療"},
-  {data_id: "0000020310", name: "Ｊ　福祉・社会保障"},
-  {data_id: "0000020311", name: "Ｋ　安全"},
+  { data_id: '0000020301', name: 'Ａ　人口・世帯' },
+  { data_id: '0000020302', name: 'Ｂ　自然環境' },
+  { data_id: '0000020303', name: 'Ｃ　経済基盤' },
+  { data_id: '0000020304', name: 'Ｄ　行政基盤' },
+  { data_id: '0000020305', name: 'Ｅ　教育' },
+  { data_id: '0000020306', name: 'Ｆ　労働' },
+  { data_id: '0000020307', name: 'Ｇ　文化・スポーツ' },
+  { data_id: '0000020308', name: 'Ｈ　居住' },
+  { data_id: '0000020309', name: 'Ｉ　健康・医療' },
+  { data_id: '0000020310', name: 'Ｊ　福祉・社会保障' },
+  { data_id: '0000020311', name: 'Ｋ　安全' }
 ]
 
 # read and join datasets
@@ -49,7 +49,7 @@ map_id_name = {}
     estat = Datasets::Estat::EstatAPI.new(dataset[:data_id], skip_nil_column: true)
     schema << estat.schema
     estat.each do |record|
-      joined_data[record.id] = [] if not joined_data.key? record.id
+      joined_data[record.id] = [] unless joined_data.key? record.id
       joined_data[record.id] += record.values
       map_id_name[record.id] = record.name
     end
@@ -67,12 +67,12 @@ end
 # do clustering
 k = 12 # Number of clusters to find
 kmeans = KMeansClusterer.run k, rows, labels: labels, runs: 100
-puts ["id", "name", "cluster"].join(",")
+puts %w[id name cluster].join(',')
 kmeans.clusters.each do |cluster|
   # puts "Cluster #{cluster.id}"
   # puts "Center of Cluster: #{cluster.centroid}"
   # puts "Cities in Cluster: " + cluster.points.map { |c| c.label }.join(",")
   cluster.points.each do |p|
-    puts [p.label, map_id_name[p.label], cluster.id].join(",")
+    puts [p.label, map_id_name[p.label], cluster.id].join(',')
   end
 end
--- red-datasets-estat-jp.gemspec
+++ red-datasets-estat-jp.gemspec
@@ -1,27 +1,27 @@
 
-lib = File.expand_path("../lib", __FILE__)
+lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "estat-jp/version"
+require 'estat-jp/version'
 
 Gem::Specification.new do |spec|
-  spec.name = "red-datasets-estat-jp"
+  spec.name = 'red-datasets-estat-jp'
   spec.version = Datasets::Estat::VERSION
-  spec.authors = ["Kunihiko MIYOSHI"]
-  spec.email = ["miyoshik@gmail.com"]
+  spec.authors = ['Kunihiko MIYOSHI']
+  spec.email = ['miyoshik@gmail.com']
 
-  spec.summary = %q{e-Stat API wrapper comform to Red Data Tools}
-  spec.description = %q{e-Stat API wrapper comform to Red Data Tools.}
-  spec.homepage = "https://github.com/colspan/red-datasets-estat-jp"
-  spec.license = "MIT"
+  spec.summary = 'e-Stat API wrapper comform to Red Data Tools'
+  spec.description = 'e-Stat API wrapper comform to Red Data Tools.'
+  spec.homepage = 'https://github.com/colspan/red-datasets-estat-jp'
+  spec.license = 'MIT'
 
   spec.files = `git ls-files -z`.split("\x0").reject do |f|
     f.match(%r{^(test|spec|features)/})
   end
-  spec.bindir = "exe"
+  spec.bindir = 'exe'
   spec.executables = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = ['lib']
 
-  spec.add_development_dependency "bundler", "~> 1.16"
-  spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "rspec", "~> 3.0"
+  spec.add_development_dependency 'bundler', '~> 1.16'
+  spec.add_development_dependency 'rake', '~> 10.0'
+  spec.add_development_dependency 'rspec', '~> 3.0'
 end
